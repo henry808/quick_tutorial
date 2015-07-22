bf663bf99c6a6f1b8ea6631f79535b7c5da1452b
Henry Grantham <hgrantham@gmail.com>
2015-07-22 02:38:29 -0700

    format tests.py and views.py

--- request_response/tutorial/views.py
+++ request_response/tutorial/views.py
@@ -14,7 +14,6 @@ class TutorialViews:
     @view_config(route_name='plain')
     def plain(self):
         name = self.request.params.get('name', 'No Name Provided')
-
         body = 'URL %s with name: %s' % (self.request.url, name)
         return Response(
             content_type='text/plain',
